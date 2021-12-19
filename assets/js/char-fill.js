53a10d4a8bb49c043f077f3a817af6cc7707c03f
chriskimball <92878250+chriskimball@users.noreply.github.com>
2021-12-19 15:20:17 -0800

    Comments on char-fill js file and style.css file, re-organized style.css to have related styling next to each other

--- assets/js/char-fill.js
+++ assets/js/char-fill.js
@@ -1,34 +1,34 @@
-var searchFormEl = $('#search-input')
-var characterFill = []
-var lotrApiKey = 'wamtzXv_h1XiQdZTQkoc'
+// Global variable and element selector
+var searchFormEl = $( '#search-input' );
+var characterFill = [];
+var lotrApiKey = 'wamtzXv_h1XiQdZTQkoc';
 
-getAllCharData()
-
+// Function to fetch all LOTR characters to populate an array for a jQueryUI autocomplete component 
 function getAllCharData () {
-
     var requestUrl = `https://the-one-api.dev/v2/character`;
-    
     var bearer = 'Bearer ' + lotrApiKey;
     
-    fetch(requestUrl , {
+    fetch( requestUrl , {
         method: 'GET',
         headers: {
             'Authorization': bearer,
             'Content-Type': 'application/json'
         }})
-        .then(function (response) {
-        
+        .then( function ( response ) {
             return response.json()
-        .then(function(data){
-            for(i=0; i < data.docs.length; i++) {
-                characterFill = characterFill.concat(data.docs[i].name)
-            }
-
-            window.characterFill = characterFill
+        .then( function( data ) {
+            // For each character name returned, add to an array
+            for( i=0; i < data.docs.length; i++ ) {
+                characterFill = characterFill.concat( data.docs[i].name );
+            };
+            // Sets character fill array as variable to the whole browser window 
+            window.characterFill = characterFill;
             searchInputEl.autocomplete({
                 source: characterFill
             });
-        
-        })
-    })
-}
+        });
+    });
+};
+
+// Calling the function to get all character data on page load
+getAllCharData();
